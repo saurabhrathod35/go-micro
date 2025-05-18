258f8fc836c8fcd2161b14ccbfdc458e24127059
Saurabh <saurabh.rathod@lendfoundry.com>
2025-05-18 23:28:22 +0530

    Add authentication service with initial database models and routing

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"authentication-service/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Started Authentication Service")
+
+	// Connect to DB
+
+	// Set up Config
+
+	app := Config{}
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
---
+++ authentication-service/go.mod
@@ -0,0 +1,10 @@
+module authentication-service
+
+go 1.18
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+)
